903282ea359659dc2ae9b6735730124ed3a228af
Mihail Petrov <mail@mihail-petrov.me>
2023-01-04 12:16:46 +0200

    к

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const express   = require('express');
 const APP       = express();
-const PORT      = 5000;
+const PORT      = process.env.PORT || 5000;
 
 APP.get('/', (req, res) => {
     res.send('Hello World');
